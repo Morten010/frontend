4c4946c4de392c1328812e59a5f41b203d85d2f3
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-06-14 22:37:53 +0200

    fix: removed deleted component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import Navbar from "./components/ui/Navbar.vue"
 import { Icon } from '@iconify/vue';
 </script>
